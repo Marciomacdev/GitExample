3f5c8a6c7028fc6a0ba7960ca0495fbde634a0dd
Marcio Santos <marciosantos@Marcios-MacBook-Air.local>
2017-04-14 09:36:28 -0300

    Create a file.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Marcio Santos on 14/04/17.
+//  Copyright © 2017 Marcio Santos. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary : NSObject {
+    
+}
